f49801010f8b68444b1dac5fc444d7e3fb89b69e
Fernando Calisaya <hit2025arg@gmail.com>
2024-07-27 00:06:19 -0300

    ruta agregada en controlador front

--- FrontEnd/src/apiController/applicantsApi.js
+++ FrontEnd/src/apiController/applicantsApi.js
@@ -24,3 +24,26 @@ export async function getApplicantById(id) {
         });
     return getData;
 }
+export async function cargaRegistro(formData) {
+  try {
+    const response = await fetch('http://localhost:3001/applicants/register', {
+      method: 'POST',
+      body: formData
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error HTTP  al registrar aspirante ,ruta sin respuesta': ${response.status} - ${response.statusText}`);
+    }
+   
+    const data = await response.json();
+    console.log('Respuesta exitosa:', data);
+    
+    if (data.error) {
+        throw new Error(`Error del servidor: ${data.error}`);
+    }
+
+  } catch (error) {
+    console.error('Error en funcion cargaRegistro:', error.message);
+    throw new Error(`Error HTTP al registrar aspirante: ${error.message}`);
+  }
+}
