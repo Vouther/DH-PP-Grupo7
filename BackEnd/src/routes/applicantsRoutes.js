112ec30712788da50c7e771d3d6f6e5f57541b10
Fernando Calisaya <hit2025arg@gmail.com>
2024-07-27 00:08:47 -0300

    route agregada al Back

--- BackEnd/src/routes/applicantsRoutes.js
+++ BackEnd/src/routes/applicantsRoutes.js
@@ -5,5 +5,6 @@ const applicantsController = require('../controllers/applicantsController');
 //API aspirantes
 router.get('/',applicantsController.renderList);
 router.get('/:id',applicantsController.renderDetail);
+router.post('/register',applicantsController.renderRegister);
 
 module.exports = router;
