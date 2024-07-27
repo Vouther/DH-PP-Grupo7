c0134b683c01aed42a5760eb77c3eee5127d04a3
Fernando Calisaya <hit2025arg@gmail.com>
2024-07-27 11:31:46 -0300

    link postular agregado a NavigationBar

--- FrontEnd/src/components/NavigationBar.jsx
+++ FrontEnd/src/components/NavigationBar.jsx
@@ -112,6 +112,14 @@ export default function Example() {
             Contacto
           </a>
         </PopoverGroup>
+        
+       <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+        <a  href="/applicants/register"
+          className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900">
+          Postulate 
+        </a>
+       </div
+         
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <a href="/error" className="text-sm font-semibold leading-6 text-gray-900">
             Acceder <span aria-hidden="true">&rarr;</span>
