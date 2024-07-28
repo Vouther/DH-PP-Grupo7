2aa9de870a6adf94a7633c6b5d00e88e450e4235
Fernando Calisaya <hit2025arg@gmail.com>
2024-07-27 22:48:24 -0300

    Agregado cargaImagenes

--- BackEnd/src/routes/applicantsRoutes.js
+++ BackEnd/src/routes/applicantsRoutes.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const applicantsController = require('../controllers/applicantsController');
 
+const multer = require('multer');
+const upload = multer({ dest: 'public/img/' }); 
+
 //API aspirantes
 router.get('/',applicantsController.renderList);
 router.get('/:id',applicantsController.renderDetail);
-router.post('/register',applicantsController.renderRegister);
+router.post('/register', upload.single('Imagen'),applicantsController.renderRegister);
 
 module.exports = router;
