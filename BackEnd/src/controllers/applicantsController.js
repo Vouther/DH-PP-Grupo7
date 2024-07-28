0cfcfa6e5ce64e638d0ab54162edf991c241502b
Vouther <lu_neji_shakugan09@hotmail.com>
2024-07-28 17:05:44 -0300

    Actualizacion de rama

--- BackEnd/src/controllers/applicantsController.js
+++ BackEnd/src/controllers/applicantsController.js
@@ -94,17 +94,17 @@ renderRegister: async (req, res) => {
       };
 
       console.log('Aspirante creado:', aspiranteCreado);
-      res.status(201).json(aspiranteCreado); 
+      res.status(201).json(aspiranteCreado);
     } catch (error) {
       console.error('Error al registrar aspirante:', error);
       res.status(500).json({ error: 'Ocurrió un error al registrar el aspirante.' });
     }
   }
-  
+
 }
 
 
-}
+
 
 
 module.exports = controller;
