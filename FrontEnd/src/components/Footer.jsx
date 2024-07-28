b55bdb57ae48f1ab3c41f2711d26e520c9b8397a
Fernando Calisaya <hit2025arg@gmail.com>
2024-07-27 23:44:40 -0300

    cambio Footer

--- FrontEnd/src/components/Footer.jsx
+++ FrontEnd/src/components/Footer.jsx
@@ -67,16 +67,7 @@ export default function Example() {
 
         <div className="mx-6 py-10 text-center md:text-left">
             <div className="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4">
-            <div className="">
-                <h6
-                className="mb-4 flex items-center justify-center font-semibold uppercase md:justify-start">
-                <img alt="" src="../../public/rrhh-gray-footer.png" className="h-8 w-auto px-2" />
-                Recursos Humanos
-                </h6>
-                <p>
-                Este es un proyecto realizado como parte de la practica profecional planteado por el equipo de Digital House. En el mismo se empleo la metodologia SCRUM.
-                </p>
-            </div>
+
             <div>
                 <h6
                 className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
@@ -97,24 +88,19 @@ export default function Example() {
             </div>
 
             <div>
-                <h6
-                className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
-                Opciones
-                </h6>
-                <p className="mb-4">
-                <a href="/applicants/register">Postularse</a>
-                </p>
-                <p className="mb-4">
-                <a href="/error">Empresas</a>
-                </p>
-                <p className="mb-4">
-                <a href="/applicants">Aspirantes</a>
-                </p>
-                <p>
-                <a href="/contact">Contacto</a>
-                </p>
+              <p className="mb-4">
+                <a href="/applicants/register" className="block px-3 py-1.5 rounded-lg bg-gray-100 text-gray-800 hover:bg-gray-200">Postularse</a>
+              </p>
+              <p className="mb-4">
+                <a href="#" className="block px-3 py-1.5 rounded-lg bg-gray-100 text-gray-800 hover:bg-gray-200">Empresas</a>
+              </p>
+              <p className="mb-4">
+                <a href="/applicants" className="block px-3 py-1.5 rounded-lg bg-gray-100 text-gray-800 hover:bg-gray-200">Aspirantes</a>
+              </p>
+              <p>
+                <a href="/contact" className="block px-3 py-1.5 rounded-lg bg-gray-100 text-gray-800 hover:bg-gray-200">Contacto</a>
+              </p>
             </div>
-
             <div>
                 <h6
                 className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
@@ -162,20 +148,7 @@ export default function Example() {
                 </span>
                 + 549 3884 567881
                 </p>
-                <p className="flex items-center justify-center md:justify-start">
-                <span className="me-3 [&>svg]:h-5 [&>svg]:w-5">
-                    <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor">
-                    <path
-                        fillRule="evenodd"
-                        d="M7.875 1.5C6.839 1.5 6 2.34 6 3.375v2.99c-.426.053-.851.11-1.274.174-1.454.218-2.476 1.483-2.476 2.917v6.294a3 3 0 003 3h.27l-.155 1.705A1.875 1.875 0 007.232 22.5h9.536a1.875 1.875 0 001.867-2.045l-.155-1.705h.27a3 3 0 003-3V9.456c0-1.434-1.022-2.7-2.476-2.917A48.716 48.716 0 0018 6.366V3.375c0-1.036-.84-1.875-1.875-1.875h-8.25zM16.5 6.205v-2.83A.375.375 0 0016.125 3h-8.25a.375.375 0 00-.375.375v2.83a49.353 49.353 0 019 0zm-.217 8.265c.178.018.317.16.333.337l.526 5.784a.375.375 0 01-.374.409H7.232a.375.375 0 01-.374-.409l.526-5.784a.373.373 0 01.333-.337 41.741 41.741 0 018.566 0zm.967-3.97a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H18a.75.75 0 01-.75-.75V10.5zM15 9.75a.75.75 0 00-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 00.75-.75V10.5a.75.75 0 00-.75-.75H15z"
-                        clipRule="evenodd" />
-                    </svg>
-                </span>
-                + 549 3884 842164
-                </p>
+
             </div>
             </div>
         </div>
